5d4f9833f147b1fbf9e69c09eb09be8d91a8f39d
Richie Bendall <richiebendall@gmail.com>
2020-12-28 04:53:32 +1300

    Minor tweaks (#4)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.js
+++ index.js
@@ -26,52 +26,45 @@ const win32 = async () => {
 };
 
 const getListFunction = process.platform === 'darwin' ? macos : (process.platform === 'linux' ? linux : win32);
-const columns = process.platform === 'darwin' ? [3, 8] : (process.platform === 'linux' ? [4, 6] : [1, 4]);
-const isProtocol = x => /^\s*(tcp|udp)/i.test(x);
+const addressColumn = process.platform === 'darwin' ? 3 : (process.platform === 'linux' ? 4 : 1);
+const portColumn = process.platform === 'darwin' ? 8 : (process.platform === 'linux' ? 6 : 4);
+const isProtocol = value => /^\s*(tcp|udp)/i.test(value);
 
 const parsePid = pid => {
 	if (typeof pid !== 'string') {
 		return;
 	}
 
-	const match = /(?:^|",|",pid=)(\d+)/.exec(pid);
-	if (!match) {
-		return;
+	const {groups} = /(?:^|",|",pid=)(?<pid>\d+)/.exec(pid);
+	if (groups) {
+		return Number.parseInt(groups.pid, 10);
 	}
-
-	return Number.parseInt(match[1], 10);
 };
 
 const getPort = (port, list) => {
 	const regex = new RegExp(`[.:]${port}$`);
-	const foundPort = list.find(x => regex.test(x[columns[0]]));
+	const foundPort = list.find(line => regex.test(line[addressColumn]));
 
 	if (!foundPort) {
 		throw new Error(`Could not find a process that uses port \`${port}\``);
 	}
 
-	return parsePid(foundPort[columns[1]]);
+	return parsePid(foundPort[portColumn]);
 };
 
 const getList = async () => {
 	const list = await getListFunction();
 
-	return list.split('\n')
-		// TODO: Remove the `.reduce`.
-		// eslint-disable-next-line unicorn/no-reduce
-		.reduce((result, x) => {
-			if (isProtocol(x)) {
-				result.push(x.match(/\S+/g) || []);
-			}
-
-			return result;
-		}, []);
+	return list
+		.split('\n')
+		.filter(line => isProtocol(line))
+		.map(line => line.match(/\S+/g) || []);
 };
 
 module.exports.portToPid = async port => {
 	if (Array.isArray(port)) {
 		const list = await getList();
-		const tuples = await Promise.all(port.map(x => [x, getPort(x, list)]));
+		const tuples = await Promise.all(port.map(port_ => [port_, getPort(port_, list)]));
 		return new Map(tuples);
 	}
 
@@ -86,10 +79,10 @@ module.exports.all = async () => {
 	const list = await getList();
 	const returnValue = new Map();
 
-	for (const x of list) {
-		const match = /[^]*[.:](\d+)$/.exec(x[columns[0]]);
-		if (match) {
-			returnValue.set(Number.parseInt(match[1], 10), parsePid(x[columns[1]]));
+	for (const line of list) {
+		const {groups} = /[^]*[.:](?<port>\d+)$/.exec(line[addressColumn]);
+		if (groups) {
+			returnValue.set(Number.parseInt(groups.port, 10), parsePid(line[portColumn]));
 		}
 	}
 
